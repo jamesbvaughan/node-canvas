dad9ae82d6f44596c92f34521c2493faaed514d8
James Vaughan <james@jamesbvaughan.com>
2024-02-22 18:33:08 -0800

    Add echo

--- prebuild/macOS/preinstall.sh
+++ prebuild/macOS/preinstall.sh
@@ -1,4 +1,5 @@
 brew update
 brew install pkg-config cairo pango librsvg python3 giflib # python3 is for macpack
 brew upgrade python # activates python 3
+echo "PIP INSTALL"
 pip3 install --break-system-packages --user macpack
